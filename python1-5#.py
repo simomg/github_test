883cfa6a1d4c9c5b17e7e9e9963ecf5a1072ada2
simomg <ssh60710@gmail.com>
2021-05-09 20:11:30 +0900

    주석

---
+++ python1-5#.py
@@ -0,0 +1,17 @@
+#주석
+# 애완동물을 소개해 주세요!
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+
+'''
+print("우리집 " + animal + "의 이름은 " + name + "에요")
+hobby = "공놀이"
+print(name + "는 " + str(age) + "살이며, " + hobby+ "을 아주 좋아해요")
+print(name + "는 어른일까요?" + str(is_adult))
+'''
+
+#주석처리 편하게 하고싶으면 문장 선택후 커맨드 슬래시 해제도 같음
