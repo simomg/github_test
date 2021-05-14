4b1a66e31704a37b3ee31316ee7bdb4aa8acd251
simomg <ssh60710@gmail.com>
2021-05-14 17:16:31 +0900

    튜플

---
+++ python4-3tuple.py
@@ -0,0 +1,14 @@
+# 튜플
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# menu.add("생선까스") // 오류 튜블은 add 불가
+
+name = "김종국"
+age = 20
+hobby = "코딩"
+print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
