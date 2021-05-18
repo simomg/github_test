38a4e9d483d5dd5766390d41e035fca75ca675e1
simomg <ssh60710@gmail.com>
2021-05-18 21:17:56 +0900

    for

---
+++ python5-2for.py
@@ -0,0 +1,13 @@
+# for
+# print("대기번호 : 1")
+# print("대기번호 : 2")
+# print("대기번호 : 3")
+# print("대기번호 : 4")
+
+# # randrange()
+# for waiting_no in range(1, 6): # 1,2,3,4,5
+#     print("대기번호 : {0}".format(waiting_no))
+
+starbucks = ["아이언맨", "토르", "아이엠그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
