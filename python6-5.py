cea8f048a269f2a68cb620b41e6d2e030060b3d9
simomg <ssh60710@gmail.com>
2021-05-20 17:00:48 +0900

    가변인자

---
+++ python6-5.py
@@ -0,0 +1,13 @@
+#가변인자
+# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
+#     print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+#     print(lan1, lang2, lang3, lang4, lang5)
+
+def profile(name, age, *language):
+    print("이름 : {0}\t나이 : {1}\t".format(name, age), end=" ")
+    for lang in language:
+        print(lang, end=" ")
+    print()
+
+print("유재석", 20, "Python", "Java", "C", "C++", "C#", "JavaScript")
+print("김태호", 25, "Kotlin", "Swift")
