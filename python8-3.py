49badfe70286754db22696c4731ce682b3ed18e7
simomg <ssh60710@gmail.com>
2021-05-28 16:27:56 +0900

    멤버 변수

---
+++ python8-3.py
@@ -0,0 +1,26 @@
+# 멤버 변수 self blahblahh 
+
+class Unit:
+    def __init__(self, name, hp, damage):
+        self.name = name
+        self.hp = hp
+        self.damage = damage
+        print("{0} 유닛이 생성 되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+
+# marine1 = Unit("마린", 40, 5)
+# marine2 = Unit("마린", 40, 5)
+# tank = Unit("탱크", 150, 35)
+
+# 레이스 : 공중 유닛, 비행기. 클로킹(싱대방에게 보이지 않음)
+wraith1 = Unit("레이스", 80, 5)
+print("유닛 이름 : {0}, 공격력 : {1}".format(wraith1.name, wraith1.damage))
+
+# 마인드 컨트롤 : 상대방 유닛을 내 것으로 만드는 것 (빼앗음)
+wraith2 = Unit("빼앗은 레이스", 80, 5)
+wraith2.clocking = True
+if wraith2.clocking == True:
+    print("{0}는 현재 클로킹 상태입니다.".format(wraith2.name))
+
+#클래스 외부에서 내가 원하는 어떤 변수에대해서 확장할수도있고 그확장된 변수는 내가 확장을 한 객체에 대해서만 적용이되고 기존에 있는 다른 객체에는 적용이 안된다는 점
