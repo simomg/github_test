7f596f220e23ead6f5325478cdecaf3df63d0c98
simomg <ssh60710@gmail.com>
2021-06-04 17:07:01 +0900

    슈퍼 super class

---
+++ python8-9super_class.py
@@ -0,0 +1,17 @@
+# 슈퍼 super class
+class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+class Flyable:
+    def __init__(self):
+        print("Flyable 생성자")
+
+class FlyableUnit(Flyable, Unit):
+    def __init__(self):
+       # super().__init__()
+        Unit.__init__(self)
+        Flyable.__init__(self)
+
+#드랍쉽
+dropship = FlyableUnit()
